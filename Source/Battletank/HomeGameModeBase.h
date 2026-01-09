ea9c91502453bca072773bc1ef7bfbdc4fdc98b2
Curry <1029061912@qq.com>
2026-01-09 17:45:50 +0800

    提交完整的空项目文件

---
+++ Source/Battletank/HomeGameModeBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HomeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API AHomeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
