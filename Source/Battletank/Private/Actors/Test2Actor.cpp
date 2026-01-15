f1113b815287b8810b4ce271774ef6187122a0d2
Curry <1029061912@qq.com>
2026-01-15 20:23:58 +0800

    feat:测试文件

---
+++ Source/Battletank/Private/Actors/Test2Actor.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Test2Actor.h"
+
+
+// Sets default values
+ATest2Actor::ATest2Actor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATest2Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATest2Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
