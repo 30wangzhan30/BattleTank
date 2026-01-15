6910f3fe573876f193ec29c11421791a1582c242
LuckFactor <2457924214@qq.com>
2026-01-15 18:13:12 +0800

    feat:游戏保存

---
+++ Source/Battletank/Private/Map/MapEditer.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Map/MapEditer.h"
+
+
+// Sets default values
+AMapEditer::AMapEditer()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMapEditer::BeginPlay()
+{
+	Super::BeginPlay();
+	DrawMapContainer();
+	
+}
+
+// Called every frame
+void AMapEditer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMapEditer::DrawMapContainer()
+{
+	UStaticMeshComponent* MapContainer = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MapContainer"));
+	UStaticMesh* MapContainerMesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Script/Engine.StaticMesh'/Engine/BasicShapes/Plane.Plane'"));
+	MapContainer -> SetStaticMesh(MapContainerMesh);
+	MapContainer -> SetWorldScale3D(FVector(MapSize.X * GridSize / 100.f, MapSize.Y * GridSize / 100.f, 1.0f));
+	MapContainer -> SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+}
+
---
+++ Source/Battletank/Public/Interface/ISaveInterface/SaveInterface.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SaveInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class USaveInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class BATTLETANK_API ISaveInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	
+	// 保存游戏状态
+	virtual void OnSave(FJsonObject& OutJson) = 0;
+	// 加载游戏状态
+	virtual void OnLoad(const FJsonObject& InJson) = 0;
+};
